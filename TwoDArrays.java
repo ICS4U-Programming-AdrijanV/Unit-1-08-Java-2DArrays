6682d8bc2bca7273d7ae340d58a65657929a134a
Adrijan Vranjkovic <adrijan.vranjkovic@stu.ocsb.ca>
2023-04-04 02:31:48 +0000

    TwoDArrays.java

--- TwoDArrays.java
+++ TwoDArrays.java
@@ -1,15 +1,15 @@
-// Import class
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public final class TwoDArrays {
 
-    static String[][] GenerateMarks(String[] studentArray, String[] assignArray) {
+    static String[][] generateMarks(String[] studentArray, String[] assignArray) {
 
         // create the 2D array.
-        String[][] studentMarks = new String[studentArray.length+1][assignArray.length+1];
+        String[][] studentMarks = new String[studentArray.length + 1][assignArray.length + 1];
 
         // Set the counter.
         int counter1 = 0;
@@ -19,17 +19,17 @@ public final class TwoDArrays {
 
         // For loop that sets the assignments in position on column 0.
         for (int position = 0; position < assignArray.length; position++) {
-            studentMarks[0][position+1] = assignArray[position];
+            studentMarks[0][position + 1] = assignArray[position];
         }
 
         // For loop that sets the student names in position.
         for (String student : studentArray) {
-            studentMarks[counter1+1][0] = student;
+            studentMarks[counter1 + 1][0] = student;
 
             // This for loop positions the student marks.
             for (int counter2 = 0; counter2 < assignArray.length; counter2++) {
-                int randomNum = (int) Math.round(Math.random()*10+75);
-                studentMarks[counter1+1][counter2+1] = String.valueOf(randomNum);
+                int randomNum = (int) Math.round(Math.random() * 10 + 75);
+                studentMarks[counter1 + 1][counter2 + 1] = String.valueOf(randomNum);
             }
             // add to the counter every time this loop goes through.
             counter1++;
@@ -37,8 +37,13 @@ public final class TwoDArrays {
         // Return the 2D loop.
         return studentMarks;
     }
-
-    public static void main(String[] args) throws Exception {
+    /**
+    * This is the main method.
+    *
+    * @param args //
+    * @throws Exception //
+    */
+    public static void main(String[] args) throws IOException {
 
         // Create and import the student, assignment, output file.
         final File students = new File("students.txt");
@@ -50,8 +55,8 @@ public final class TwoDArrays {
         final Scanner scAssignments = new Scanner(assignments);
 
         // Create the array lists.
-        final ArrayList<String> studentList = new ArrayList<String>();
-        final ArrayList<String> assignmentList = new ArrayList<String>();
+        final ArrayList<String> studentList = new ArrayList<>();
+        final ArrayList<String> assignmentList = new ArrayList<>();
 
         // Use this string to fill the arrayList.
         String str1;
@@ -94,12 +99,12 @@ public final class TwoDArrays {
         }
 
         // Call the generate marks function.
-        String[][] marks = GenerateMarks(studentArray, assignArray);
+        final String[][] marks = generateMarks(studentArray, assignArray);
 
         // This loop displays the 2D array.
-        for (int row = 0; row < marks.length; row++) {
-            for (int column = 0; column < marks[row].length; column++) {
-                output.write(marks[row][column] + ", ");
+        for (String[] mark : marks) {
+            for (String s : mark) {
+                output.write(s + ", ");
             }
             output.write("\n");
         }
